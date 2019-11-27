eede7d4ec89c68da49c4e5a5c2a178191b19d6af
Low-class-dev <laper91@gmail.com>
2019-11-27 16:16:35 +0100

    added search options

--- src/js/search.js
+++ src/js/search.js
@@ -1,13 +1,25 @@
-const searchButton = document.querySelector('.search');
-const input = document.querySelector('input')
+const searchButton = document.querySelector('.search-btn');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const select = document.getElementById('search-value')
+let query;
 
 const search = (e) => {
     e.preventDefault()
+    query = select.options[select.selectedIndex].value
     const task = input.value;
     console.log(task)
-    axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${task}`)
-        .then((response) => {
-            console.log(response)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${query}:"${task}"`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Content-Type-Options': 'nosniff'
+            }
+        })
+        .then((data) => {
+            const li = document.createElement('li');
+            li.innerHTML = data
+            ul.appendChild(li)
+            console.log(data)
         })
         .catch((error) => {
             console.log(error);
