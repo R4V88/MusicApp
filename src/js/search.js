dc7cb91a10b9a3c07c14f9746fd22f3392a10af3
Sabina <sabina.sadowska@gmail.com>
2019-12-01 12:45:09 +0100

    loop working

--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,9 @@
 const searchButton = document.querySelector('.search-btn');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
-const select = document.getElementById('search-value')
-let query;
+const select = document.getElementById('search-value');
+
+
 
 const search = (e) => {
     e.preventDefault()
@@ -15,31 +16,44 @@ const search = (e) => {
             }
         })
         .then((data) => {
-            console.log(data)
-            function createResultsArtist() {
-
-                let myObj = {
-                    artist: data.data.data[i].artist.name,
-                    artist: data.data.data[i].picture_small,
-                    title: data.data.data[i].title,
-                    music: data.data.data[i].previev
-                }
-                var i;
-                for (i=0; i<data.length;i++){
-                console.log(data.data.data[i].artist.name);
-                console.log(data.data.data[i].picture_small);
-                console.log(data.data.data[i].previev);
-                console.log(data.data.data[i].title);
-    
-                console.log(data)
-
+            console.log(data);
+            let x = data;
+            for (i=0; i<x.data.data.length;i++){
+                console.log(x.data.data[i].artist.name);
+                console.log(x.data.data[i].picture_small);
+                console.log(x.data.data[i].previev);
+                console.log(x.data.data[i].title);
             
-                 }} ;
+            
+            
+                 }
+                 
         })
         .catch((error) => {
             console.log(error);
         })
 }
+
+
+
+function createResultsArtist() {
+    let myObj = {
+        artist: data.data.data[i].artist.name,
+        artist: data.data.data[i].picture_small,
+        title: data.data.data[i].title,
+        music: data.data.data[i].previev
+    }
+    var i;
+    for (i=0; i<data.length;i++){
+    console.log(getDataFromApi.data.data[i].artist.name);
+    console.log(data.data.data[i].picture_small);
+    console.log(data.data.data[i].previev);
+    console.log(data.data.data[i].title);
+    console.log(myObj.artist);
+
+
+
+     }};
 
 
 searchButton.addEventListener('click', search)
