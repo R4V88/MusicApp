81c274eb8cc28bf64a34f841d10ce7795a675b6e
Sabina <sabina.sadowska@gmail.com>
2019-12-03 12:11:42 +0100

    removed let x and left data only

--- src/js/search.js
+++ src/js/search.js
@@ -16,30 +16,29 @@ const search = (e) => {
             }
         })
         .then((data) => {
-            let x = data;
             document.getElementById("search_results").innerHTML="";
-            for (i=0; i<x.data.data.length;i++){
+            for (i=0; i<data.data.data.length;i++){
                 var div = document.createElement('div');
                 div.id = "card"
-                if(x.data.data[i].artist.picture_medium === "https://cdns-images.dzcdn.net/images/artist//250x250-000000-80-0-0.jpg" || x.data.data[i].artist.picture_medium ==="https://e-cdns-images.dzcdn.net/images/artist//250x250-000000-80-0-0.jpg"){
+                if(data.data.data[i].artist.picture_medium === "https://cdns-images.dzcdn.net/images/artist//250x250-000000-80-0-0.jpg" || data.data.data[i].artist.picture_medium ==="https://e-cdns-images.dzcdn.net/images/artist//250x250-000000-80-0-0.jpg"){
                 div.innerHTML = `<div id="photo_wrapper">
-                    <img id="photo" src="${x.data.data[i].album.cover_medium}" alt="Cover photo">
+                    <img id="photo" src="${data.data.data[i].album.cover_medium}" alt="Cover photo">
                 </div>
                 <div id="info_wrapper">
-                    <p id="artist_name">${x.data.data[i].artist.name}</p>
-                    <p id="title">${x.data.data[i].title}</p>
-                    <p id="album">Album: ${x.data.data[i].album.title}</p>
-                    <audio controls><source src="${x.data.data[i].preview}"></audio>
+                    <p id="artist_name">${data.data.data[i].artist.name}</p>
+                    <p id="title">${data.data.data[i].title}</p>
+                    <p id="album">Album: ${data.data.data[i].album.title}</p>
+                    <audio controls><source src="${data.data.data[i].preview}"></audio>
                 </div>`;} 
                 else {
                 div.innerHTML = `<div id="photo_wrapper">
-                    <img id="photo" src="${x.data.data[i].artist.picture_medium}" alt="Artist photo">
+                    <img id="photo" src="${data.data.data[i].artist.picture_medium}" alt="Artist photo">
                 </div>
                 <div id="info_wrapper">
-                    <p id="artist_name">${x.data.data[i].artist.name}</p>
-                    <p id="title">${x.data.data[i].title}</p>
-                    <p id="album">Album: ${x.data.data[i].album.title}</p>
-                    <audio controls><source src="${x.data.data[i].preview}"></audio></div>`;}
+                    <p id="artist_name">${data.data.data[i].artist.name}</p>
+                    <p id="title">${data.data.data[i].title}</p>
+                    <p id="album">Album: ${data.data.data[i].album.title}</p>
+                    <audio controls><source src="${data.data.data[i].preview}"></audio></div>`;}
                 document.getElementById("search_results").appendChild(div)
                 }    
         })
